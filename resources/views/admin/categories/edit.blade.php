7db9474b0e393421f4ddf613769a2e3c0d069312
Haquangtuananh <duyanhlama@gmail.com>
2022-04-04 14:38:50 +0700

    feat: categories, brands, suppliers, products cms

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -2,19 +2,19 @@
 
 
 @section('content')
-<h1 class="h3 mb-2 text-gray-800">Sửa danh mục</h1>
+<h1 class="h3 mb-2 text-gray-800">Cập nhật danh mục</h1>
 
 <div class="row">
     <div class="col-lg-12">
-        <form action="{{ route('category.edit',['id' => $category->id_dmsp]) }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('category.edit',['id' => $category->id]) }}" method="POST" enctype="multipart/form-data">
 
             @csrf
 
             <div class="form-group">
                 <label for="name">Tên danh mục: <span class="text-danger">*</span></label>
-                <input type="text" class="form-control" placeholder="Nhập tên danh mục" id="name" name="name" value="{{ $category->ten_dmsp }}" required>
+                <input type="text" class="form-control" placeholder="Nhập tên danh mục" id="name" name="name" value="{{ $category->name }}" required>
             </div>
-            <button type="submit" class="btn btn-primary">Sửa</button>
+            <button type="submit" class="btn btn-primary">Cập nhật</button>
           </form>
     </div>
 </div>
