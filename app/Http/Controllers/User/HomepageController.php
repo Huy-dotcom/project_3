d1688d96ed7cb4581f6f098248e99d20527f69c1
Huy-dotcom <77112505+Huy-dotcom@users.noreply.github.com>
2022-04-05 15:10:09 +0700

    nothing

--- app/Http/Controllers/User/HomepageController.php
+++ app/Http/Controllers/User/HomepageController.php
@@ -9,9 +9,9 @@ use Illuminate\Http\Request;
 class HomepageController extends Controller
 {
     public function index(){
-        $saleList = Product::whereNotNull('sale_price')->get();
+        $saleList = Product::where('sale_price', '!=' , 0)->Where('sale_price','!=',null)->get();
         $saleListCount = Product::whereNotNull('sale_price')->count();
-        $productInOrder = Product::orderBy('updated_at','DESC');
+        $productInOrder = Product::orderBy('updated_at','DESC')->get();
         return view('user.homepage',[
             'saleList' => $saleList,
             'saleListCount' => $saleListCount,
