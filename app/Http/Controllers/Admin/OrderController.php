54585cde3e84d925507c9671bcb3176cbfa13396
Haquangtuananh <duyanhlama@gmail.com>
2022-04-15 01:58:09 +0700

    orders cms, revenue

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\OrderDetail;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Order::join('users','orders.user_id','=','users.id')->get(['orders.*','users.name']);
+        return view('admin.orders.list', compact('orders'));
+    }
+
+    public function show($id)
+    {
+        $orders_detail = OrderDetail::where('order_id',$id)
+        ->join('products','products.id','=','order_details.product_id')
+        ->get(['order_details.*','products.name','products.price']);
+        $order = Order::find($id);
+        return view('admin.orders.show', compact('orders_detail', 'order'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $order = Order::find($id);
+        $order->status = $request->status;
+        $order->save();
+        return redirect()->route('order.list')->with('success', 'Cập nhật trạng thái thành công.');
+    }
 }
--- resources/views/admin/layouts/index.blade.php
+++ resources/views/admin/layouts/index.blade.php
@@ -168,6 +168,10 @@
         </div>
     </div>
 
+    <input type="hidden" id="data1" value="{{ json_encode($data1) }}" />
+    <input type="hidden" id="data2" value="{{ json_encode($data2) }}" />
+    <input type="hidden" id="data3" value="{{ json_encode($data3) }}" />
+
     <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
@@ -294,6 +298,80 @@
             }
 
             return true;
+        }
+    </script>
+    {{-- Google Charts Library --}}
+    <script src="https://www.gstatic.com/charts/loader.js"></script>
+    {{-- Revenue by day --}}
+    <script type="text/javascript">
+        var arr = [['Ngày', 'Doanh thu']];
+        var orders = JSON.parse(document.getElementById("data1").value);
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+        for(x of orders){
+          arr.push([x.order_day,parseInt(x.total_price)])
+        }  
+        function drawChart() {
+  
+          var data = google.visualization.arrayToDataTable(
+             arr
+          );
+  
+          var options = {
+            title: 'Thống kê doanh thu theo ngày'
+          };
+  
+          var chart = new google.visualization.PieChart(document.getElementById('piechart1'));
+  
+          chart.draw(data, options);
+        }
+    </script>
+    {{-- Revenue by month --}}
+    <script>
+        var arr1 = [['Tháng', 'Doanh thu']];
+        var orders = JSON.parse(document.getElementById("data2").value);
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+        for(x of orders){
+          arr1.push(['Tháng '+x.order_month,parseInt(x.total_price)])
+        }  
+        function drawChart() {
+  
+          var data = google.visualization.arrayToDataTable(
+              arr1
+          );
+  
+          var options = {
+            title: 'Thống kê doanh thu theo tháng'
+          };
+  
+          var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+  
+          chart.draw(data, options);
+        }
+    </script>
+    {{-- Revenue by year --}}
+    <script>
+        var arr2 = [['Năm', 'Doanh thu']];
+        var orders = JSON.parse(document.getElementById("data3").value);
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+        for(x of orders){
+          arr2.push(['Năm '+x.order_year,parseInt(x.total_price)])
+        }  
+        function drawChart() {
+  
+          var data = google.visualization.arrayToDataTable(
+              arr2
+          );
+  
+          var options = {
+            title: 'Thống kê doanh thu theo năm'
+          };
+  
+          var chart = new google.visualization.PieChart(document.getElementById('piechart3'));
+  
+          chart.draw(data, options);
         }
     </script>
 </body>
